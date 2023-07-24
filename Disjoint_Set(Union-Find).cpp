5c880aacf762ac44bf74351871675bdea2609a10
Mohammad Abdul Hadi <45118566+mdabdulHadi@users.noreply.github.com>
2023-07-24 10:58:00 +0530

    Create Disjoint_Set(Union-Find).cpp

---
+++ Disjoint_Set(Union-Find).cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class DisjointSet {
+private:
+    vector<int> parent;
+    vector<int> rank;
+
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+public:
+    DisjointSet(int size) {
+        parent.resize(size);
+        rank.resize(size, 0);
+        for (int i = 0; i < size; ++i) {
+            parent[i] = i;
+        }
+    }
+
+    void Union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+            } else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+        }
+    }
+
+    bool isConnected(int x, int y) {
+        return find(x) == find(y);
+    }
+};
