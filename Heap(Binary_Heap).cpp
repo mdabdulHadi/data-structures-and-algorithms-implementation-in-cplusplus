a8a23b02a684b3c7415b46fe7300bb8232c93dfd
Mohammad Abdul Hadi <45118566+mdabdulHadi@users.noreply.github.com>
2023-07-24 10:43:19 +0530

    Create Heap(Binary_Heap).cpp

---
+++ Heap(Binary_Heap).cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Heap {
+private:
+    vector<int> heap;
+    int size;
+
+    void heapifyUp(int idx) {
+        // Fix the heap property by moving the element up
+    }
+
+    void heapifyDown(int idx) {
+        // Fix the heap property by moving the element down
+    }
+
+public:
+    Heap() : size(0) {
+        heap.push_back(0); // The 0th index is not used to simplify calculations
+    }
+
+    void insert(int val) {
+        heap.push_back(val);
+        ++size;
+        heapifyUp(size);
+    }
+
+    int extractMin() {
+        if (size == 0) {
+            return -1; // Assuming -1 represents an empty heap
+        }
+        int minVal = heap[1];
+        heap[1] = heap[size];
+        heap.pop_back();
+        --size;
+        heapifyDown(1);
+        return minVal;
+    }
+
+    bool isEmpty() {
+        return size == 0;
+    }
+};
